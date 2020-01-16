f03efec215236d6dcd0bd84ff38f594f93cb0a86
Shaqqour <mshaqqourm@gmail.com>
2020-01-16 15:17:35 -0800

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,5 +1,5 @@
 class SongsController < ApplicationController
-    before_action :set_song!, only: [:show, :edit, :update]
+    before_action :set_song!, only: [:show, :edit, :update, :destroy]
 
     def index
         @songs = Song.all
@@ -12,13 +12,35 @@ class SongsController < ApplicationController
         @song = Song.new
     end
 
+    def create
+        @song = Song.new(song_params)
+        if @song.save
+            redirect_to song_path(@song)
+        else
+            render :new
+        end
+    end
+
     def edit
+    end
+
+    def update
+        if @song.update(song_params)
+            redirect_to song_path(@song)
+        else
+            render :edit
+        end
+    end
+
+    def destroy
+        @song.delete
+        redirect_to songs_path
     end
     
     private
     
     def song_params
-        params.permit()
+        params.require(:song).permit(:title, :release_year, :released, :genre, :artist_name)
     end
     def set_song!
         @song = Song.find(params[:id])
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,6 +1,13 @@
 class Song < ApplicationRecord
     validates :title, presence: true
-    validates_uniqueness_of :title, scope: [:artist_name, :release_year]
-    validates :release_year, presence: true, only: :released?
-    validates :release_year, numericality: { less_than_or_equal_to: Time.now.year }
+    validates_uniqueness_of :title, scope: [:artist_name, :release_year], message: 'cannot be repeated by the same artist in the same year'
+    validates :released, inclusion: { in: [true, false] }
+    validates :artist_name, presence: true
+    validates :release_year, presence: true, if: :released?
+    validates :release_year, numericality: { less_than_or_equal_to: Time.now.year }, if: :released?
+
+    def released?
+        released
+    end
+
 end
